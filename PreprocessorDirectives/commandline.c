e201575c3834ddefb786a1cb496935a407d477bb
VrushabBayas <rushab.bayas@gmail.com>
2018-06-03 10:21:27 +0530

    e

--- PreprocessorDirectives/commandline.c
+++ PreprocessorDirectives/commandline.c
@@ -1,3 +1,12 @@
+/*
+ * Filename: commandline.c
+ * Created Date: Saturday, June 2nd 2018, 11:39:51 pm
+ * Author: Vrushab Bayas
+ * @description 
+ * Print command line arguments
+ * Copyright (c) 2018 Your Company
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
